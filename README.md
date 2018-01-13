8d3cc31fe43ed74fd1dae39907591cae0f99d7ed
Good Beard <goodbeard@Goods-MacBook-Pro.local>
2018-01-13 09:33:12 -0500

    working model

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # LIRI
-Language Interpretation Recognition Interface
+# Language Interpretation Recognition Interface
+
+LIRI is a command line interface for node.  Taking input from user prompts, LIRI will search, spotify, OMDB, and Twitter returning song data, movie data, and recent tweets.
+
+## Getting Started
+
+*Note: This assumes that you already have Node.js installed locally.*
+
+1. Clone the repo to your local machine. [Git Repo](https://github.com/iangoodnight/LIRI.git)
+1. Run ```npm install``` to get required packages.
+1. You will need to provide your own ```.env``` file with Twitter and Spotify API keys.
+	```
+	# Spotify API keys (replace with your key/secret)
+	SPOTIFY_ID=your-spotify-id
+	SPOTIFY_SECRET=your-spotify-secret
+	# Twitter API keys (replace with your keys/secrets)
+	TWITTER_CONSUMER_KEY=your-twitter-consumer-key
+	TWITTER_CONSUMER_SECRET=your-twitter-consumer-secret
+	TWITTER_ACCESS_TOKEN_KEY=your-access-token-key
+	TWITTER_ACCESS_TOKEN_SECRET=your-twitter-access-token-secret
+	```
+
+1. From the root folder, run LIRI!
+
+
+
--- liri.js
+++ liri.js
@@ -1,15 +1,161 @@
-//  .env package containing Twitter and Spotify API keys
 require("dotenv").config();
-//  keys package
 var keys = require ('./keys.js');
-
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-
-var fs = require('fs');
-
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+var inquirer = require('inquirer');
 var omdb = require('omdb');
-
 var request = require('request');
+
+
+function introduction() {
+	console.log("Hey Kiddo!  My name is Liri and I can do stuff!");
+}
+
+var runLoop = function() {
+	inquirer
+		.prompt([
+		{
+			type: "confirm",
+			message: "Anything else?",
+			name: "restart"
+		}
+		]).then(function(data) {
+			console.log("data.restart: " + data.restart);
+			if (data.restart === true) {
+				run();
+			}
+		});
+};
+
+//  Get 20 Latest Tweets
+function getTweets(name) {
+
+	var Name = name;
+	var params = {screen_name: Name};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (error) {
+			console.log('Error occurred: ' + error);
+		} else {
+			var tweetLength = tweets.length;
+			for (var i = 0; i < tweetLength && i < 20; i++) {
+	    	console.log(tweets[i].text);
+	  		}
+	  	runLoop();
+		}
+	});
+}
+
+//  Get first five spotify results
+function getSpotify(song) {
+	var Song = song;
+	spotify.search({ type: 'track', query: Song }, function(err, data) {
+	    if ( err ) {
+	       	console.log('Error occurred: ' + err);
+	       	return;
+	   	} else {
+	   		var results = data.tracks.items;
+    		for (var j = 0; j < results.length && j < 5; j++) {
+				for (var k = 0; k < results[j].album.artists.length; k++) {
+					console.log("Artist(s): " + results[j].album.artists[k].name);
+				}
+			console.log("Song: " + results[j].name + "\nPreview: " + results[j].preview_url + "\nAlbum: " + results[j].album.name + "\n\n--- --- ---\n");
+	    	}
+	    runLoop();
+		}
+	 });
+}
+
+//  Get OMDB results
+function getOMDB(input) {
+	if (!input) {
+		input = "Mr. Nobody";
+	} else {
+		input = input;
+	}
+	var queryUrl = 'http://www.omdbapi.com/?apikey=trilogy&t=' + encodeURIComponent(input);
+		
+	request(queryUrl, function(error, response, body) {
+		body = JSON.parse(body);
+		if (error) {
+			console.log("Error: " + error);
+		} else {
+			console.log("\nTitle: " + body.Title + "\nReleased: " + body.Released + "\nIMDB Rating: " + body.Ratings[0].Value + "\nRotten Tomatoes Rating: " + body.Ratings[1].Value + "\nCountry Produced: " + body.Country + "\nLanguage: " + body.Language + "\nPlot: " + body.Plot + "\nActors: " + body.Actors);
+			runLoop();
+		}
+	});
+}
+
+
+
+
+function run() {
+
+	inquirer
+		.prompt([
+		{
+			type: "input",
+			message: "Whacha wanna know?",
+			name:  "action"
+		}
+		])
+		.then(function(data) {
+		if (data.action === "my-tweets") {
+			inquirer
+				.prompt([
+				{
+					type: "input",
+					message: "Whazzer handle muchacho? (Don't worry, if you aren't twitter savvy you can just leave the field blank and check out my sweet, sweet tweets!) \n --->",
+					name: "handle"
+				}
+				])
+				.then(function(data) {
+					if (!data.handle) {
+						getTweets("chirptobesquare");
+					} else {
+					getTweets(data.handle);
+					}
+				});
+		}
+		if (data.action === "spotify-this-song") {
+			inquirer
+				.prompt([
+				{
+					type: "input",
+					message: "Got a song stuck in your head? (If not, you can leave the field blank and I'll pick a song to stick in your head!) \n--->",
+					name: "song"
+				}
+				])
+				.then(function(data) {
+					if (!data.song) {
+						getSpotify("The Sign (Ace of Base)");
+					} else {
+						getSpotify(data.song);
+					}
+				});
+		}
+		if (data.action === "movie-this") {
+			inquirer
+				.prompt([
+				{
+					type: "input",
+					message: "I love movies!  Whaddya been watchin'? (If you leave the input blank, I can tell you about what I've been whatchin'!)",
+					name: "movie"
+				}
+				])
+				.then(function(data) {
+					console.log("data.movie: " + data.movie);
+					getOMDB(data.movie);
+				});
+		}
+	});
+}
+
+introduction();
+run();
+
+
+	
+
+
